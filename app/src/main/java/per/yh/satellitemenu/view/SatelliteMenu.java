e004d477337c13552a1727be6d719ed3e4d08398
MoonLeo <yanhan_90@126.com>
2016-01-03 21:47:41 +0800

    实现自定义属性

--- app/src/main/java/per/yh/satellitemenu/view/SatelliteMenu.java
+++ app/src/main/java/per/yh/satellitemenu/view/SatelliteMenu.java
@@ -1,7 +1,9 @@
 package per.yh.satellitemenu.view;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.util.TypedValue;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,12 +16,14 @@ import android.view.animation.TranslateAnimation;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
+import per.yh.satellitemenu.R;
+
 /**
  * Created by MoonLeo on 2015/12/29.
  * 卫星菜单View类
  */
 public class SatelliteMenu extends ViewGroup implements View.OnClickListener {
-
+    private static final String TAG = SatelliteMenu.class.getSimpleName();
 
     /**
      * 枚举菜单可能的位置
@@ -29,7 +33,7 @@ public class SatelliteMenu extends ViewGroup implements View.OnClickListener {
     }
 
     private Position mPosition;//菜单在屏幕上的位置
-    private int mRadio;//菜单半径
+    private int mRadius;//菜单半径
     private boolean isOpen;//菜单打开状态
 
     private RelativeLayout mainBtn;//主按钮
@@ -47,8 +51,32 @@ public class SatelliteMenu extends ViewGroup implements View.OnClickListener {
 
     public SatelliteMenu(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        mRadio = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 150, getResources().getDisplayMetrics());
-        mPosition = Position.LEFT_BOTTOM;
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.satellitemenu);
+        int r = (int) a.getDimension(R.styleable.satellitemenu_radius, 150);
+        mRadius = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, r, getResources().getDisplayMetrics());
+        int pos = a.getInt(R.styleable.satellitemenu_position, 0);
+        Log.d(TAG, pos+"");
+        switch (pos) {
+            case 0:
+                mPosition = Position.LEFT_TOP;
+                break;
+            case 1:
+                mPosition = Position.LEFT_BOTTOM;
+                break;
+            case 2:
+                mPosition = Position.RIGHT_TOP;
+                break;
+            case 3:
+                mPosition = Position.RIGHT_BOTTOM;
+                break;
+            default:
+                mPosition = Position.LEFT_TOP;
+                break;
+        }
+        a.recycle();
+        Log.d(TAG, mPosition+"");
+
+
     }
 
     @Override
@@ -150,8 +178,8 @@ public class SatelliteMenu extends ViewGroup implements View.OnClickListener {
         double angle = Math.PI/(2 * (subMenuCount-1));//子菜单之间的夹角
         int x , y;//子菜单的左上坐标
         for(int i=0; i< subMenuCount; i ++) {
-            x = l +  (int) (mRadio *Math.cos(startAngle));
-            y = t + (int) (mRadio*Math.sin(startAngle));
+            x = l +  (int) (mRadius *Math.cos(startAngle));
+            y = t + (int) (mRadius *Math.sin(startAngle));
             ImageView subMenuItem = (ImageView) getChildAt(i+1);
             subMenuItem.setVisibility(View.GONE);
             subMenuItem.layout(x, y, x + subMenuItem.getMeasuredWidth(), y + subMenuItem.getMeasuredHeight());
